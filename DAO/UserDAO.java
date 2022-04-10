0a5796a825b7869019529bd80adb4365cf3e6663
assafi2 <99564058+assafi2@users.noreply.github.com>
2022-04-10 08:17:05 +0300

    Delete DAO directory

--- DAO/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import PersistantDomainObjects.User;
-
-public interface UserDAO extends DAO<User> {
-
-    public String retRoleName(int role_id) ;
-    public User getUserByUsername(String userName) ;
-}
