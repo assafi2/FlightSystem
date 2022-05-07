09970d07534ce41c7fb3b89f8456637e792587e0
assafi2 <99564058+assafi2@users.noreply.github.com>
2022-05-07 03:44:40 +0300

    Delete src/main/java/com/example/FlightSystemBackend/MongoDBRelies directory

--- src/main/java/com/example/FlightSystemBackend/MongoDBRelies/repository/AddOpsRepositoy.java
+++
@@ -1,8 +0,0 @@
-package com.example.FlightSystemBackend.MongoDBRelies.repository;
-
-import com.example.FlightSystemBackend.MongoDBRelies.DTO.AddOps;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AddOpsRepositoy extends MongoRepository<AddOps,Integer> {
-
-}
